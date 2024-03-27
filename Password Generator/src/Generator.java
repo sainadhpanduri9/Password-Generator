664b448fa3b7361305244c041c96561bdc711e0d
sainadhpanduri9 <132693783+sainadhpanduri9@users.noreply.github.com>
2024-03-27 17:29:07 +0530

    Update Generator.java

--- Password Generator/src/Generator.java
+++ Password Generator/src/Generator.java
@@ -52,7 +52,7 @@ public class Generator {
         final int alphabetLength = alphabet.getAlphabet().length();
 
         int max = alphabetLength - 1;
-        int min = 0;
+        int min=0;
         int range = max - min + 1;
 
         for (int i = 0; i < length; i++) {
@@ -65,12 +65,12 @@ public class Generator {
 
     private void printUsefulInfo() {
         System.out.println();
-        System.out.println("Use a minimum password length of 8 or more characters if permitted");
-        System.out.println("Include lowercase and uppercase alphabetic characters, numbers and symbols if permitted");
+        System.out.println("Use a minimum password of length 8 or more characters ");
+        System.out.println("Include lowercase and uppercase alphabetic characters, numbers and symbols ");
         System.out.println("Generate passwords randomly where feasible");
         System.out.println("Avoid using the same password twice (e.g., across multiple user accounts and/or software systems)");
         System.out.println("Avoid character repetition, keyboard patterns, dictionary words, letter or number sequences," +
-                "\nusernames, relative or pet names, romantic links (current or past) " +
+                "\usernames, relative or pet names, romantic links (current or past) " +
                 "and biographical information (e.g., ID numbers, ancestors' names or dates).");
         System.out.println("Avoid using information that the user's colleagues and/or " +
                 "acquaintances might know to be associated with the user");
